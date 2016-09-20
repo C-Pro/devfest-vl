81b1cab71239670e147c69765ea198afa8eaf1e0
Sergey Melekhin <sergey@melekhin.me>
2016-09-20 18:47:19 +1000

    Added ru locale, localized most of main page

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,8 @@ module.exports = function(grunt) {
         app: {
             source: 'app',
             dist: 'dist',
-            baseurl: 'zeppelin-grunt',
-            git_repo: 'git@github.com:gdg-x/zeppelin-grunt.git',
+            baseurl: 'f',
+            git_repo: 'git@github.com:C-Pro/devfest-vl.git',
             branch: 'gh-pages'
         },
         watch: {
